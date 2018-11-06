85f20c63e0af3ef1a0789cb2d7dfdfc4607469e2
Christopher Beckham <chrispy645@gmail.com>
2018-11-06 18:40:49 -0500

    use_tgt_dir option

--- FaceWarper/warp_dataset.py
+++ FaceWarper/warp_dataset.py
@@ -20,12 +20,16 @@ class Timer:
         return time.perf_counter() - self._start_time
 
 class Dataset:
-    def __init__(self, dataset_dir, affine_identity_path):
+    def __init__(self, dataset_dir, affine_identity_path, use_tgt_dir=False):
         self._dataset_dir = dataset_dir
         self._affine_identity_path = affine_identity_path
+        if use_tgt_dir:
+            self.use_folder = "tgt_images"
+        else:
+            self.use_folder = "source"
 
     def identity_iterator(self):
-    	for f in os.listdir(os.path.join(self._dataset_dir, "source")):
+    	for f in os.listdir(os.path.join(self._dataset_dir, self.use_folder)):
     		yield extract_identity(f)
 
     def source_filepath(self, fileID):
@@ -79,10 +83,16 @@ def warp_all(server, dataset, results_destination, options):
     processed_count = 0
     total_timer = Timer()
     for i, fileID in enumerate(dataset.identity_iterator()):
+
+        print(fileID)
+        
         if i < options.start_index:
             continue
 
-        image = dataset.source_filepath(fileID)
+        if options.img_override is not None:
+            image = options.img_override
+        else:
+            image = dataset.source_filepath(fileID)
         keypoints = dataset.keypoints_filepath(fileID)
         depth = dataset.depth_filepath(fileID)
 
@@ -91,7 +101,17 @@ def warp_all(server, dataset, results_destination, options):
         else:
             affine = dataset.affine_filepath(fileID)
 
+
         result = results_destination.result_filepath(fileID)
+
+        #print(image)
+        #print(keypoints)
+        #print(depth)
+        #print(affine)
+
+        #import sys
+        #sys.exit(0)
+        
  
         if not all_exist([image, keypoints, depth, affine]):
             print("Skipping : %s" % (fileID,))
@@ -110,13 +130,15 @@ def parse_args():
     parser.add_argument('--server_exec', default=None, help='Path to the server executable. If not set, the program assumes the server executable is named FaceWarperServer and is locating in system PATH.')
     parser.add_argument('--identity', action='store_true', help='Use identity affine transform.')
     parser.add_argument('--start_index', type=int, default=0, help='Index of the first image to warp in the dataset.')
+    parser.add_argument('--img_override', type=str, default=None, help='If set, use this specified image instead of those in the source folder')
+    parser.add_argument('--use_tgt_dir', action='store_true', help='If set, find file ids using the target folder, instead of source')
     args = parser.parse_args()
     return args
 
 def main():
     options = parse_args()
     affine_identity_filepath = test_create_affine_identity_file()
-    dataset = Dataset(options.dataset_path, affine_identity_filepath)
+    dataset = Dataset(options.dataset_path, affine_identity_filepath, options.use_tgt_dir)
     results_destination = ResultsDestination(options.results)
     with fwc.Server(options.server_exec) as server:
         warp_all(server, dataset, results_destination, options)
